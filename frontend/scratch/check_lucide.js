cb4e8284889ca962dfe050c8e6447d40ca017263
Manohar Singh <manohar72singh@gmail.com>
2026-04-30 01:01:59 +0530

    fix: remove unused configuration file to clean up project structure

--- frontend/scratch/check_lucide.js
+++
@@ -1,4 +0,0 @@
-import * as Lucide from 'lucide-react';
-console.log('MapPin exists:', !!Lucide.MapPin);
-console.log('MapPinned exists:', !!Lucide.MapPinned);
-console.log('Available icons count:', Object.keys(Lucide).length);
